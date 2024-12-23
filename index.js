d058e2570894489e2f3df677530497669b4e520b
Davis Inde Satya <davis.onde.wei@gmail.com>
2024-12-23 12:08:42 +0700

    try serverless for netlify

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { AuthRoute, SpotifyRoute } from "./route/Spotify.js"
+import ServerlessHttp from "serverless-http"
 
 export const Config = {
     port: "3030",
@@ -13,6 +14,5 @@ app.get(SpotifyRoute.Route, SpotifyRoute.Controller)
 app.get(AuthRoute.Route, AuthRoute.Controller)
 
 console.log("Starting server...")
-app.listen(Config.port, () => {
-    console.log(`Server started at ${Config.port}`)
-})
+
+ServerlessHttp(app)
