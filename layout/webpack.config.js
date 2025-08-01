3cfe930637c666636361880006967df21e5a1610
EbelAlfie <davis.onde.wei@gmail.com>
2025-08-02 00:12:53 +0700

    try css loader

--- layout/webpack.config.js
+++ layout/webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
     open: true,
     port: 3000,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
